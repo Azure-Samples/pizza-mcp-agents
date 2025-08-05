60a9158e69b0677453829c381849c1bfbdb2db86
sinedied <noda@free.fr>
2025-08-05 11:25:38 +0200

    chore: update packages

--- src/pizza-mcp/src/config.ts
+++ src/pizza-mcp/src/config.ts
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // Env file is located in the root of the repository
-dotenv.config({ path: path.join(__dirname, "../../../.env") });
+dotenv.config({ path: path.join(__dirname, "../../../.env"), quiet: true });
 
 export const pizzaApiUrl = process.env.PIZZA_API_URL || "http://localhost:7071";
